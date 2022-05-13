a9f4b33257e3b1db0413e6da6bb25c291a91737c
August Ollé Corbella <atorremedia@gmail.com>
2022-05-13 10:57:16 +0200

    arrow

--- js/parallax.js
+++ js/parallax.js
@@ -1,12 +1,15 @@
 let layer1 = document.querySelector('.layer1');
 let layer2 = document.querySelector('.layer2');
 let layer3 = document.querySelector('.layer3');
+let arrow = document.querySelector('.arrow');
 
 function scrollParallax(){
     let scrollTop = document.documentElement.scrollTop;
     layer1.style.transform = 'translateY('+ scrollTop * -.2 +'px)';
     layer2.style.transform = 'translateY('+ scrollTop * -1.4 +'px)';
     layer3.style.transform = 'translateY('+ scrollTop * -2.5 +'px)';
+    if (scrollTop > 20){arrow.style.display = "none"}
+    else {arrow.style.display = "block"}
     svgAnimate();
 }
 const svg = document.querySelectorAll('svg');
